2faac4b4c9c370c7c3a01d0664b2cd6f74e5ee8e
liyuqing <misezhiyan@163.com>
2020-03-15 12:34:56 +0800

    项目初始化

---
+++ src/main/java/com/maicai/CaiyuanMaiCaiWeb.java
@@ -0,0 +1,13 @@
+package com.maicai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaiyuanMaiCaiWeb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaiyuanMaiCaiWeb.class, args);
+	}
+
+}
---
+++ src/main/java/com/maicai/web/TestController.java
@@ -0,0 +1,15 @@
+package com.maicai.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test() {
+
+		return "index";
+	}
+}
